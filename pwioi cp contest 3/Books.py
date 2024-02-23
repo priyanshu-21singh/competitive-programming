5c29a8e8990928923dd12c9330d4f9654f5f70b5
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-02-23 23:42:57 +0530

    Add files via upload

---
+++ pwioi cp contest 3/Books.py
@@ -0,0 +1,25 @@
+def max_books(n, t, books):
+    left = 0
+    right = 0
+    max_books_count = 0
+    current_time = 0
+
+    while right < n:
+        current_time += books[right]
+
+        while current_time > t:
+            current_time -= books[left]
+            left += 1
+
+        max_books_count = max(max_books_count, right - left + 1)
+        right += 1
+
+    return max_books_count
+
+#  input
+n, t = map(int, input().split())
+books = list(map(int, input().split()))
+
+# Output the result
+result = max_books(n, t, books)
+print(result)
