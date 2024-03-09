45d0922f1a4fd3b3a8c214f5f7e2d8f7f65ad497
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-03-09 21:10:57 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/Balanced round.py
@@ -0,0 +1,27 @@
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    ans = 1
+    local = 1
+    for i in range(1, n):
+        if abs(a[i] - a[i - 1]) <= k:
+            local += 1
+        else:
+            ans = max(ans, local)
+            local = 1
+
+    a.sort()
+    local = 1
+
+    for i in range(1, n):
+        if a[i] - a[i - 1] <= k:
+            local += 1
+        else:
+            ans = max(ans, local)
+            local = 1
+
+    ans = max(ans, local)
+    print(n - ans)
