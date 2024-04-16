ed3626df0471bb6d8379dbe94a7f00caa5642802
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-04-16 22:57:59 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/A.InSearchofanEasyProblem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n; // Input a value for 'n'
+    int sum = 0;
+    for(int i = 0;i < n; i++){
+        int x;
+        cin>>x;
+        sum+=x;
+    }
+
+    if(sum){
+        cout<<"HARD" << endl;
+    }else{
+        cout<<"EASY" << endl;
+    }
+    return 0;
+}
