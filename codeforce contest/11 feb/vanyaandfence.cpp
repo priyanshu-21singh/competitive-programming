57319ef761a4eafb22e14798670e248975aac4cf
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-04-16 22:58:24 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/vanyaandfence.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,h,a, count = 0;
+    cin>>n>>h;
+    for(int i = 0;i < n; i++) {
+        cin>>a;
+        if(a>h){
+            count+=2;
+        }else{
+            count++;
+        }
+
+
+    }
+    cout<<count<<endl;
+    return 0;
+
+}
