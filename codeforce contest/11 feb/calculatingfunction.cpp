9069ebeb757e401d0564bfe07653d263a20491cd
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-04-18 22:56:24 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/calculatingfunction.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main () {
+    long long n,a;
+    cin>>n;
+    if(n%2==0){
+        a = n/2;
+    }else{
+        a = -((n+1)/2);
+
+    }
+    cout<<a<<endl;
+    return 0;
+
+}
