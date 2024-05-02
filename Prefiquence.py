dd3e7ad2184d5be89d2dfa33274321e7b305d687
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-05-02 22:52:19 +0530

    Add files via upload

---
+++ Prefiquence.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    a = input()
+    b = input()
+    
+    ans = 0
+    i = 0
+    for j in range(len(b)):
+        if b[j] == a[i]:
+            ans += 1
+            i += 1
+        if i == n:
+            break
+    
+    print(ans)
