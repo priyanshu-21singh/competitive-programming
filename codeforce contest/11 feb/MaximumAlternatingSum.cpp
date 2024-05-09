7b700824c3a51d4014cf170a5178ae7ae09eacc7
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-05-09 09:08:35 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/MaximumAlternatingSum.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve() {
+	int n;
+	cin >> n;
+	int arr[n], summ = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+       
+    // first sort the error...
+	sort(arr, arr + n);
+	if (n % 2 != 0) {
+		for (int i = 0; i < n; i++) {
+			if (i < n / 2) {
+				summ -= arr[i];
+			}
+			else {
+				summ += arr[i];
+			}
+            
+		}
+	}
+    // use loop 
+	else {
+		for (int i = 0; i < n; i++) {
+			if (i < n / 2) {
+				summ -= arr[i];
+			}
+			else {
+				summ += arr[i];
+			}
+
+		}
+	}
+	cout << summ << endl;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+}
