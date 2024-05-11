c7c31498dc7b45d12d1844c45251f04bb7d94543
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-05-11 10:10:01 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/Myfirstsorting.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+
+        // Swap x and y if x is greater than y
+        if (x > y) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        // Output the minimum of x and y followed by the maximum of x and y
+        cout << x << " " << y << endl;
+    }
+
+    return 0;
+}
