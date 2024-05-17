0c82596613375d057b4629fb257e52b88501622b
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-05-17 10:10:58 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/JaggedSwaps.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int Min = INT_MAX;
+        for(int i=0;i<n;i++){
+            if(arr[i]<Min){
+                Min = arr[i];
+            }
+        }if(Min == arr[0]){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
