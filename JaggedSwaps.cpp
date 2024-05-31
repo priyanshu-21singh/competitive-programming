30857c6d139d2d6d538ba9945c278eba01746528
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-05-31 17:29:57 +0530

    Update JaggedSwaps.cpp

--- JaggedSwaps.cpp
+++ JaggedSwaps.cpp
@@ -8,7 +8,7 @@ int main(){
         cin>>n;
         int arr[n];
         for(int i=0;i<n;i++){
-            cin>>arr[i];
+            cin>>arr[i];//
         }
         int Min = INT_MAX;
         for(int i=0;i<n;i++){
