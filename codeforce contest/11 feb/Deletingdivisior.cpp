c93fcefb10ffea225c704dfc169eb85f3b80f187
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-06-25 12:58:39 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/Deletingdivisior.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    // cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n % 2 != 0) {
+            cout << "Bob\n";
+        } else {
+            int count = 0;
+            while (n % 2 == 0) {
+                n /= 2;
+                count++;
+            }
+            if (n > 1) {
+                cout << "Alice\n";
+            } else {
+                if (count % 2 != 0) {
+                    cout << "Bob\n";
+                } else {
+                    cout << "Alice\n";
+                }
+            }
+        }
+    }
+    return 0;
+}
