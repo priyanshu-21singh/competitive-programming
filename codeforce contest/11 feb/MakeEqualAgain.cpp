4717e984ac815dbd4027ee2cda274a01bf4a8db7
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-07-02 23:41:58 +0530

    Update MakeEqualAgain.cpp

--- codeforce contest/11 feb/MakeEqualAgain.cpp
+++ codeforce contest/11 feb/MakeEqualAgain.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main(){
 int t;
-    cin >> t;
+cin >> t;
 
-    while(t--){
-        int n;
-        cin >> n;
+while(t--){
+    int n;
+    cin >> n;
 
         vector<int>arr(n);
         for(int i=0; i<n; i++) cin >> arr[i];
