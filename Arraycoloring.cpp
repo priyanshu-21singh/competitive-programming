8fe9f6b3472e73a3101d186b157bdd7dd7cbd606
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-07-05 09:54:21 +0530

    Update Arraycoloring.cpp

--- Arraycoloring.cpp
+++ Arraycoloring.cpp
@@ -10,7 +10,6 @@ int main(){
         vector<int> arr(n);
         int even = 0;
         int odd = 0;
-        
         for (int i=0 ;i<n;i++){
             cin>>arr[i];
             if(arr[i]%2 == 0){
