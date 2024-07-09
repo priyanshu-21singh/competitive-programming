a5c5fd1f328ace76b3fd6f19dfd0fa2beaa6a8cf
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-07-09 14:12:29 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/3sum.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    // author me priyanshu on 8-07-2024
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        // this part will keep or store remainder
+        vector<int> priyanshu;
+        for(int j : a) {
+            if(count(priyanshu.begin(),priyanshu.end(),j%10)<3){
+                priyanshu.push_back(j%10);
+            }
+        }
+        bool cat = true;
+        for(int j=0;j<priyanshu.size();j++){
+            for(int k = j+1;k<priyanshu.size();k++){
+                for (int l = k + 1; l <priyanshu.size(); l++){
+                    if((priyanshu[j]+priyanshu[k]+priyanshu[l]%10)==3) {
+                        if(cat) {
+                            cout<<"YES"<< endl;
+                        }
+                        cat = false;
+                        break;
+                    }
+                }
+                if(!cat) break;
+            }
+            if(!cat) break;
+        }
+        if(cat){
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
