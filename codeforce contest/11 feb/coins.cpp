72de844fb8a99aaf4b046649708df6f1ca4ce71d
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-08-20 13:03:54 +0530

    Add files via upload

--- codeforce contest/11 feb/coins.cpp
+++ codeforce contest/11 feb/coins.cpp
@@ -1,20 +1,14 @@
-#include <iostream>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int main () {
+int main (){
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        long long a;
-        long long b;
-        cin>>a>>b;
-        if(b==1 || b==a){cout<<"YES"<<endl;continue;}
-        if((a%2!=0 && b%2==0) || (a%2 !=0&&b>a)){
-            cout<<"No"<<endl;continue;
+        cin>>n;
+        for(int i=n;i>0;--i){
+            if(n%i==0){
+                n=i;
+                cout << n << " ";
 
+            }
         }
-        cout<<"YES"<<endl;
+        return 0;
     }
-    return 0;
-}
