97624d607bb53051a12aa6882124d9dbe7499ff4
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-08-25 10:50:13 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/AB-flipling.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t; 
+    while(t--){
+        int n;
+        cin >> n; 
+        string s;
+        cin >> s; 
+
+        int l = 0, r = s.length() - 1;
+
+        while(l < s.length() && s[l] == 'B'){
+            l++;
+        }
+        while(r >= 0 && s[r] == 'A'){
+            r--;
+        }
+
+        
+        cout << max(0, r - l) << endl;
+    }
+    return 0;
+}
