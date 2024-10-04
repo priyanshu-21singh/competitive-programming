daba3085cbe10dec1fa60dfc2ba508d056432b9f
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-10-04 09:42:46 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/XXXXX.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, x, n, a;
+    cin >> t;
+    while (t--) {
+        cin >> n >> x;
+        int sum = 0, l = -1;
+        for (int i = 0; i < n; i++) {
+            cin >> a;
+            sum += a;
+            if (sum % x != 0) l = max(l, max(i + 1, n - (i + 1)));
+        }
+        cout << l << endl;
+    }
+}
