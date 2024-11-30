47046371e1e9320de4203969ae7d6736b4ea2784
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-11-30 19:33:06 +0530

    Add files via upload

---
+++ codeforce contest/11 feb/Equal-XOR.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+    int n ,k;
+    cin>>n>>K;
+    vector<int> a(2*n);
+    for(int i=0;i<2*n;i++){
+        cin>>a[i];
+    }
+    vector<int> l,r;
+    map<int,int> freq;
+    }
+}
