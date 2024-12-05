3e63ed9f408cf58ee5f485f712aa093c92f8590c
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2024-12-05 21:56:04 +0530

    Update 3sum.cpp

--- codeforce contest/11 feb/3sum.cpp
+++ codeforce contest/11 feb/3sum.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main () { 
-    // author me priyanshu on 8-07-2024
+int main () { // author me priyanshu on 8-07-2024
     int t;
     cin>>t;
     while(t--){
