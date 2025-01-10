2d7caa31de0b8f738e729bfff99f45693cb701a5
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2025-01-10 23:42:05 +0530

    Add files via upload

--- codeforce contest/11 feb/3sum.cpp
+++ codeforce contest/11 feb/3sum.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main () { // author me priyanshu on 8-07-2024
-    
+int main () {
+    // author me priyanshu on 8-07-2024
     int t;
     cin>>t;
     while(t--){
