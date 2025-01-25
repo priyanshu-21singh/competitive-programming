3a6a9d5699cea10bc3e01ca90a0984e824601d5c
Priyanshu Singh <154409968+priyanshu-21singh@users.noreply.github.com>
2025-01-25 23:49:15 +0530

    Add files via upload

--- codeforce contest/11 feb/A.InSearchofanEasyProblem.cpp
+++ codeforce contest/11 feb/A.InSearchofanEasyProblem.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int main() {
     int n;
-    cin >> n; 
+    cin >> n; // Input a value for 'n'
     int sum = 0;
     for(int i = 0;i < n; i++){
         int x;
         cin>>x;
         sum+=x;
-        
     }
 
     if(sum){
